66c8d83fce6e3776f5cd2dd1d8ab221b7284d2ef
IvanReneUFPS <frankjeanpierregs@ufps.edu.co>
2020-12-16 18:55:43 -0500

    haciend actualizar producto

---
+++ src/public/js/fetchUpdateProduct.js
@@ -0,0 +1,94 @@
+var boolIni = false;
+
+const button = document.querySelector(`[data-type-button="form"]`);
+button.addEventListener("click", async () => {
+    if (!boolIni) {
+        button.setAttribute("disabled", "true");
+        const form = document.forms.updateProduct;
+        const id_categoria = form.querySelector("input[name='id_categoria']")
+            .dataset.idValue;
+        const id_marca = form.querySelector("input[name='id_marca']").dataset
+            .idValue;
+        const titulo = form.querySelector("input[name='titulo']").value;
+        const stock = form.querySelector("input[name='stock']").value;
+        const precio = form.querySelector("input[name='precio']").value;
+        const descripcion = form.querySelector("textarea[name='descripcion']")
+            .value;
+        const detalle = form.querySelector("textarea[name='detalle']").value;
+        const imagen = form.querySelector(`input[data-id-image='${0}']`).value;
+        const images = [];
+        for (let i = 1; i <= 4; i++) {
+            if (document.querySelector(`input[data-id-image='${i}']`).value) {
+                images.push(
+                    document.querySelector(`input[data-id-image='${i}']`).value
+                );
+            }
+        }
+        const producto = {
+            id_categoria,
+            id_marca,
+            titulo,
+            descripcion,
+            detalle,
+            precio,
+            stock,
+            imagen,
+            images,
+        };
+        let bool = true;
+        for (const field in producto) {
+            if (
+                !producto[field] ||
+                producto[field] === "undefined" ||
+                producto[field] === ""
+            ) {
+                bool = false;
+                break;
+            }
+        }
+        if (bool && !boolIni) {
+            boolIni = true;
+            const data = await fetch(
+                `${location.origin}/seller/update-product/`,
+                {
+                    method: "post",
+                    body: JSON.stringify(producto),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            const res = await data.json();
+            if (res.ok) {
+                Swal.fire({
+                    icon: "success",
+                    title: "ok!",
+                    confirmButtonText: "Ok",
+                    text: "Producto registrado correctamente",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.replace(`${location.origin}/seller/`);
+                    }
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    confirmButtonText: "Ok",
+                    text: "Ha ocurrido un error inesperado",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.replace(`${location.origin}/seller/`);
+                    }
+                });
+            }
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Debes llenar todos los campos!",
+            });
+            button.removeAttribute("disabled", "false");
+        }
+    }
+});
--- src/public/js/manageProduct.js
+++ src/public/js/manageProduct.js
@@ -1,24 +1,30 @@
-let btnShow = document.querySelector('#butAplicar');
-let selectUser = document.querySelector("[name='selectIdProduct']"); 
+let btnShow = document.querySelector("#butAplicar");
+let selectUser = document.querySelector("[name='selectIdProduct']");
 
-function obtenerCheck(){
-    const x = document.querySelectorAll('input:checked');
-    return Array.from(x).forEach((el=>el.value));
+Array.from(document.getElementsByClassName("products-check")).forEach((el) => {
+    el.addEventListener("change", () => {
+        console.log(el.dataset.state);
+        if (el.dataset.state == "En revisión") {
+            error("No puedes editar un producto en estado de revisión");
+            el.checked = false;
+        }
+    });
+});
+
+function obtenerCheck() {
+    const x = document.querySelectorAll("input:checked");
+    return Array.from(x).forEach((el) => el.value);
 }
 
-
-function selectProduct(){
-   fetch(`${location.origin}/seller/my-products/`, {
+function selectProduct() {
+    fetch(`${location.origin}/seller/my-products/`, {
         method: "post",
-        body: JSON.stringify({ 
-            
-                id_estado:document.getElementById('#selectIdProduct').value,
-                productos: obtenerCheck()
-         }),
+        body: JSON.stringify({
+            id_estado: document.getElementById("#selectIdProduct").value,
+            productos: obtenerCheck(),
+        }),
         headers: {
             "Content-Type": "application/json",
         },
-    })
-
+    });
 }
-
